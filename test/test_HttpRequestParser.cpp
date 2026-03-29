1d9bbd3c7585faf65565544b3d3c99c38c28355b
viodid <david.yunta.aller@gmail.com>
2026-03-29 13:46:06 +0200

    handle test exceptions

--- test/test_HttpRequestParser.cpp
+++ test/test_HttpRequestParser.cpp
@@ -1,42 +1,31 @@
-/*
- *
- * // Test: Good GET Request line
- * r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n "))
- * require.NoError(t, err)
- * require.NotNil(t, r)
- * assert.Equal(t, "GET", r.RequestLine.Method)
- * assert.Equal(t, "/", r.RequestLine.RequestTarget)
- * assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
- * // Test: Good GET Request line with path
- * r,err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n "))
- * require.NoError(t, err)
- * require.NotNil(t, r)
- * assert.Equal(t, "GET", r.RequestLine.Method)
- * assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
- * assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
- *
- *  // Test: Invalid number of parts in request line
- * err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n "))
- * require.Error(t, err)
- */
-
 #include "../include/HttpRequestParser.hpp"
+#include <exception>
 #include <gtest/gtest.h>
 
 TEST(HttpParserTest, ParseRequestLineCorrect)
 {
-    HttpRequest request = HttpRequestParser("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n").parse();
-    EXPECT_EQ("GET", request.getRequestLine().getMethod());
-    EXPECT_EQ("/", request.getRequestLine().getRequestTarget());
-    EXPECT_EQ("1.1", request.getRequestLine().getHttpVersion());
+    try {
+        HttpRequest request = HttpRequestParser("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n").parse();
+        EXPECT_EQ("GET", request.getRequestLine().getMethod());
+        EXPECT_EQ("/", request.getRequestLine().getRequestTarget());
+        EXPECT_EQ("1.1", request.getRequestLine().getHttpVersion());
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        FAIL();
+    }
 }
 
 TEST(HttpParserTest, ParseRequestLineCorrectWithPath)
 {
-    HttpRequest request = HttpRequestParser("GET /coffee HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n ").parse();
-    EXPECT_EQ("GET", request.getRequestLine().getMethod());
-    EXPECT_EQ("/coffee", request.getRequestLine().getRequestTarget());
-    EXPECT_EQ("1.0", request.getRequestLine().getHttpVersion());
+    try {
+        HttpRequest request = HttpRequestParser("GET /coffee HTTP/1.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: \r\n\r\n ").parse();
+        EXPECT_EQ("GET", request.getRequestLine().getMethod());
+        EXPECT_EQ("/coffee", request.getRequestLine().getRequestTarget());
+        EXPECT_EQ("1.0", request.getRequestLine().getHttpVersion());
+    } catch (std::exception& e) {
+        std::cerr << e.what() << '\n';
+        FAIL();
+    }
 }
 
 TEST(HttpParserTest, ParseRequestLineInvalid)
