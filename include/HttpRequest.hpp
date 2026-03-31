49e4419f99b9e8fe7732b6cc200224260a2ed877
viodid <david.yunta.aller@gmail.com>
2026-03-31 15:16:38 +0200

    use an struct for HttpRequest

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -1,54 +1,27 @@
 #pragma once
 #include <map>
 #include <string>
-#include <vector>
 
-class HttpRequestLine {
-public:
-    HttpRequestLine(const std::string&, const std::string&, const std::string&);
-
-    const std::string& getMethod() const;
-    const std::string& getRequestTarget() const;
-    const std::string& getHttpVersion() const;
-
-private:
-    std::string method_;
-    std::string request_target_;
-    std::string http_version_;
+struct HttpRequestLine {
+    std::string method;
+    std::string request_target;
+    std::string http_version;
 };
 
-// TODO: unused class
-class HttpFieldLine {
-public:
-    HttpFieldLine(const std::string&, const std::string&);
-
-    const std::string& getFieldName() const;
-    const std::string& getFieldValue() const;
-
-private:
-    std::string field_name_;
-    std::string field_value_;
+struct HttpFieldLine {
+    std::string field_name;
+    std::string field_value;
 };
 
-/*
- * Data holder class - it represents an HTTP 1.0 or 1.1 request
+// represents an HTTP 1.0 or 1.1 request
+/* An HTTP request is composed of:
+ * A request-line: GET /foo HTTP/1.1
+ * Field lines: Host: example.com
+ * And an optional message body followed by a new line '\r\n'
+ * https://www.rfc-editor.org/rfc/rfc9112#name-field-syntax
  */
-class HttpRequest {
-public:
-    /* An HTTP request is composed of:
-     * A request-line: GET /foo HTTP/1.1
-     * Field lines: Host: example.com
-     * And an optional message body followed by a new line '\r\n'
-     * https://www.rfc-editor.org/rfc/rfc9112#name-field-syntax
-     */
-    HttpRequest(const HttpRequestLine&, const std::map<std::string, std::string>&, const std::string&);
-
-    const HttpRequestLine& getRequestLine() const;
-    const std::map<std::string, std::string>& getFieldNames() const;
-    const std::string& getBody() const;
-
-private:
-    HttpRequestLine request_line_;
-    std::map<std::string, std::string> field_lines_;
-    std::string body_;
+struct HttpRequest {
+    HttpRequestLine request_line;
+    std::map<std::string, std::string> field_lines;
+    std::string body;
 };
